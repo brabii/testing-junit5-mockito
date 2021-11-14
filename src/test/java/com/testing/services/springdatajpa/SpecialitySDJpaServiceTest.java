fcea636defd03ddf52a2ff8df9f89fa1c7e25e9c
brabii <collectorsgarbages@gmail.com>
2021-11-14 22:38:37 +0100

    utilisation de la méthode statique verify

--- src/test/java/com/testing/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/com/testing/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.testing.model.Speciality;
@@ -20,6 +21,44 @@ public class SpecialitySDJpaServiceTest {
 	@Test
 	void testDeleteById() {
 		service.deleteById(1L);
+		/**
+		 * verifier que la méthode deleteById est invoké la methode verify doit contenir
+		 * le mock
+		 */
+		verify(specialtyRepository).deleteById(1L);
+	}
+
+	@Test
+	void testDeleteByIdTwoTimes() {
+		service.deleteById(1L);
+		service.deleteById(1L);
+		verify(specialtyRepository, times(2)).deleteById(1L);
+	}
+
+	@Test
+	void testDeleteByIdThreeTimes() {
+		service.deleteById(1L);
+		service.deleteById(1L);
+		service.deleteById(1L);
+		verify(specialtyRepository, times(3)).deleteById(1L);
+	}
+
+	@Test
+	void testDeleteByIdAtLeast() {
+		service.deleteById(1L);
+		service.deleteById(1L);
+		service.deleteById(1L);
+		verify(specialtyRepository, atLeast(3)).deleteById(1L);
+	}
+	@Test
+	void testDeleteByIdAtMost() {
+		service.deleteById(1L);
+		service.deleteById(1L);
+		verify(specialtyRepository, atMost(3)).deleteById(1L);
+	}
+	@Test
+	void testDeleteByIdAtNever() {
+		verify(specialtyRepository, never()).deleteById(1L);
 	}
 
 	@Test
