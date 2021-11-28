a403ef843d8642f0d15abe3a4815cc11a1312c3b
brabii <collectorsgarbages@gmail.com>
2021-11-28 16:09:07 +0100

    methode de test qui permet la creation d'un mock implementation de map avec les annotations mockito

---
+++ src/test/java/com/testing/IntitializeMocksWithAnnotationTest.java
@@ -0,0 +1,31 @@
+package com.testing;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class IntitializeMocksWithAnnotationTest {
+	/**
+	 * declarer le mock de classe qu'on veut moquer c'est comme declarer un objet
+	 * comme un bean
+	 */
+	@Mock
+	Map<String, Object> mapMock;
+
+	/**
+	 * initialiser les mocks de la classe en cours sans les initiliaser on aura pas
+	 * un mock implementation et par la suite un NullPointerException
+	 */
+	@BeforeEach
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	void should_create_mock_implementation_of_map_with_annotation() throws Exception {
+		mapMock.put("key", "value");
+	}
+}
